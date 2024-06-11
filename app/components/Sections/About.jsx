abe01901eaa99e2d22554dd1bf63f1163afd052b
Ayesha Afroza Mohsin <ayeshaafroza@iut-dhaka.edu>
2024-06-12 02:16:52 +0600

    added animation

--- app/components/Sections/About.jsx
+++ app/components/Sections/About.jsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <section id="about">
+    <section id="about" className="rounded-2xl bg-sky-950 p-4 mb-4">
       <h1 className="font-bold text-lg">ABOUT</h1>{" "}
       <main className="p-4">
         <h2>Ayesha Afroza Mohsin</h2>
