3d14e2152199f070b01d5db7f47be83dcc1538a0
Ayesha Afroza Mohsin <ayeshaafroza@iut-dhaka.edu>
2024-06-12 02:27:21 +0600

    Awards

--- app/components/Sections/Awards.jsx
+++ app/components/Sections/Awards.jsx
@@ -1,13 +1,45 @@
 export default function Awards() {
   return (
     <section id="awards">
-      <h1 className="font-bold text-lg">AWARDS</h1>{" "}
-      <main className="p-4">
-        <h2>Ayesha Afroza Mohsin here</h2>
-        <p>
-          An enthusiastic coder, with a passion for trying new technologies.
-        </p>
-      </main>
+      <h1 className="font-bold text-lg">AWARDS</h1>
+      <ul className="grid grid-cols-2 lg:grid-cols-3">
+        <li className="p-4 m-4 dark:bg-teal-800 bg-green-200 rounded-3xl">
+          <h2 className="text-lg font-bold">Code Odyssey by ItVerse</h2>
+          <h3>2nd Runner Up</h3>
+          <p className="text-sm">Dhaka University (DU) · Oct 2023</p>
+        </li>
+        <li className="p-4 m-4 dark:bg-teal-800 bg-green-200 rounded-3xl">
+          <h2 className="text-lg font-bold">
+            CodeRush 1.0 Programming Contest
+          </h2>
+          <h3>1st Runner Up</h3>
+          <p className="text-sm">Islamic University of Technology · Mar 2023</p>
+        </li>
+        <li className="p-4 m-4 dark:bg-teal-800 bg-green-200 rounded-3xl">
+          <h2 className="text-lg font-bold">
+            Intra IUT Girls Programming Competition
+          </h2>
+          <h3>1st Runner Up</h3>
+          <p className="text-sm">Islamic University of Technology · Sep 2022</p>
+        </li>
+        <li className="p-4 m-4 dark:bg-teal-800 bg-green-200 rounded-3xl">
+          <h2 className="text-lg font-bold">Luna Shamsuddoha Dev Mania</h2>
+          <h3>1st Runner Up</h3>
+          <p className="text-sm">May 2023</p>
+        </li>
+        <li className="p-4 m-4 dark:bg-teal-800 bg-green-200 rounded-3xl">
+          <h2 className="text-lg font-bold">
+            CodeRush 1.0 Software Project Showcasing
+          </h2>
+          <h3>Winner</h3>
+          <p className="text-sm">Islamic University of Technology · Mar 2023</p>
+        </li>
+        <li className="p-4 m-4 dark:bg-teal-800 bg-green-200 rounded-3xl">
+          <h2 className="text-lg font-bold">Intra IUT Programming Contest</h2>
+          <h3>11th Position</h3>
+          <p className="text-sm">Islamic University of Technology · Dec 2021</p>
+        </li>
+      </ul>
     </section>
   );
 }
